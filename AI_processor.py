a896054b73bdff34d253b4331e9d7de02e733561
andy9310 <r12921a25@ntu.edu.tw>
2023-12-15 10:39:41 +0000

    finish api and simple memory

--- AI_processor.py
+++ AI_processor.py
@@ -1,11 +1,11 @@
 import openai
 import json
-
+from conversation_ai import summary_processor
 #### openai processor
-def GPT_processor(message, function_description):
+def GPT_processor(length, message, function_description):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        max_tokens=2000,
+        max_tokens=length,
         temperature=0.5,
         messages=[{"role": "user", "content": message}],
         functions=function_description,
@@ -21,12 +21,15 @@ def story_creater():
     with open('./story_background/story_background_description.json',encoding="utf-8") as file:
         story_background_description = [json.load(file)]
     print("start process AI api")
-    story_response = GPT_processor(start_prompt, story_background_description)
+    story_response = GPT_processor(2000,start_prompt, story_background_description)
     # store the story dictionary result to story.json
     story_object  = json.dumps(story_response.choices[0].message.function_call.arguments)
     print(story_object.encode('ascii').decode('unicode-escape'))
     with open("./story_background/story.json", "w") as outfile:
         outfile.write(story_object)
+    f = open("./conversation_file/conversation.txt", "w",encoding="utf-8")
+    f.write(story_response.choices[0].message.function_call.arguments)
+    f.close()
     return story_response.choices[0].message.function_call.arguments
 
 
@@ -34,15 +37,22 @@ def story_creater():
 def suspect_creater(target,action):
     #### load start prompt
     f = open('./suspect_file/suspect_prompt.txt',encoding="utf-8")
+    f2 = open("./conversation_file/conversation.txt",encoding="utf-8")
     suspect_prompt = f.read()
-    suspect_prompt = suspect_prompt + " 你現在是嫌疑人:" + target +"，請回答以下問題：" + action
+    conversation_history = f2.read()
+    suspect_prompt = conversation_history + suspect_prompt + " 你現在是嫌疑人:" + target +"，請回答以下問題：" + action
     with open('./suspect_file/suspect_description.json',encoding="utf-8") as file:
         suspect_description = [json.load(file)]
-    suspect_response = GPT_processor(suspect_prompt, suspect_description)
+    suspect_response = GPT_processor(200,suspect_prompt, suspect_description)
     
     
-    suspect_object  = json.dumps(suspect_response.choices[0].message.function_call.arguments)
-    print(suspect_object.encode('ascii').decode('unicode-escape'))
+    # suspect_object  = json.dumps(suspect_response.choices[0].message.function_call.arguments)
+    f = open("./conversation_file/conversation.txt", "a",encoding="utf-8")
+    f.write(json.loads(suspect_response.choices[0].message.function_call.arguments).get("回覆"))
+    f.close()
+    summary_processor()
+    print(json.loads(suspect_response.choices[0].message.function_call.arguments).get("回覆"))
+    # print(suspect_object.encode('ascii').decode('unicode-escape'))
     # return suspect_response.choices[0].message.content
     return json.loads(suspect_response.choices[0].message.function_call.arguments).get("回覆")
     # store the reply
@@ -54,13 +64,20 @@ def suspect_creater(target,action):
 def scene_creater(action):
     #### load start prompt
     f = open('./scene_file/scene_prompt.txt',encoding="utf-8")
+    f2 = open("./conversation_file/conversation.txt",encoding="utf-8")
     scene_prompt = f.read()
-    scene_prompt = scene_prompt + "，回答以下問題:" + action
+    conversation_history = f2.read()
+    scene_prompt = conversation_history + scene_prompt + "，回答以下問題:" + action
     with open('./scene_file/scene_description.json',encoding="utf-8") as file:
         scene_description = [json.load(file)]
-    scene_response = GPT_processor(scene_prompt, scene_description)
-    scene_object  = json.dumps(scene_response.choices[0].message.function_call.arguments)
-    print(scene_object.encode('ascii').decode('unicode-escape'))
+    scene_response = GPT_processor(200,scene_prompt, scene_description)
+    # scene_object  = json.dumps(scene_response.choices[0].message.function_call.arguments)
+    f = open("./conversation_file/conversation.txt", "a",encoding="utf-8")
+    f.write(json.loads(scene_response.choices[0].message.function_call.arguments).get("回覆"))
+    print(json.loads(scene_response.choices[0].message.function_call.arguments).get("回覆"))
+    f.close()
+    summary_processor()
+    # print(scene_object.encode('ascii').decode('unicode-escape'))
     return json.loads(scene_response.choices[0].message.function_call.arguments).get("回覆")
     # print(scene_response)
     # print(scene_response.choices[0].message.content)
@@ -70,13 +87,20 @@ def scene_creater(action):
 def final_answer_creater(id, motivation, action):
     #### load start prompt
     f = open('./final_answer_file/final_answer_prompt.txt',encoding="utf-8")
+    f2 = open("./conversation_file/conversation.txt",encoding="utf-8")
     fa_prompt = f.read()
+    conversation_history = f2.read()
+    fa_prompt = conversation_history + fa_prompt
     with open('./final_answer_file/final_answer_description.json',encoding="utf-8") as file:
         final_answer_description = [json.load(file)]
-    final_answer_response = GPT_processor(fa_prompt, final_answer_description)
+    final_answer_response = GPT_processor(800,fa_prompt, final_answer_description)
     # store the story dictionary result to story.json
-    final_answer_object  = json.dumps(final_answer_response.choices[0].message.function_call.arguments)
-    print(final_answer_object.encode('ascii').decode('unicode-escape'))
+    # final_answer_object  = json.dumps(final_answer_response.choices[0].message.function_call.arguments)
+    f = open("./conversation_file/conversation.txt", "a",encoding="utf-8")
+    f.write(json.loads(final_answer_response.choices[0].message.function_call.arguments).get("真相"))
+    f.close()
+    # summary_processor()
+    # print(final_answer_object.encode('ascii').decode('unicode-escape'))
     return json.loads(final_answer_response.choices[0].message.function_call.arguments).get("真相")
     
 
--- coversation_ai.py
+++
@@ -1,3 +0,0 @@
-import openai
-
-### 為了做測試遊玩過程的AI
